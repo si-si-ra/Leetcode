335c7e349e050ce5f9c1b3d3b909ba1dd0072be2
si-si-ra <165571542+si-si-ra@users.noreply.github.com>
2025-03-10 22:11:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (53.28%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        sum = 0
+        prevValue = 0
+        value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        
+        for c in s:
+            currentValue = value[c]
+            sum += (currentValue - 2 * prevValue) if (currentValue > prevValue) else currentValue
+            prevValue = currentValue
+        return sum
+        
