2911edacd76a07c512c25a5c9e02e2974e16dcf0
si-si-ra <165571542+si-si-ra@users.noreply.github.com>
2025-04-10 06:58:41 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (89.94%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,9 +5,16 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j] == target:
-                    return [i,j]
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i]+nums[j] == target:
+        #             return [i,j]
+        # return []
+        numMap = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in numMap:
+                return [numMap[complement], i]
+            numMap[num] = i
         return []
         
